fabaa6a57de01b2622ae0a53e2175c86975bcfff
DanyJovel <40212365+DanyJovel@users.noreply.github.com>
2018-07-05 19:02:45 -0500

    Update matrixtarea.cpp

--- matrixtarea.cpp
+++ matrixtarea.cpp
@@ -14,10 +14,10 @@ char GetChar(int iGenerator, char cBase, int iRange) {
 int main() {
 	string contrasena;
 	
-	cout << "Eres un agente? ingresa tu contraseña ";
+	cout << "Eres un agente? ingresa tu contraseÃ±a ";
 	cin >> contrasena;
 	
-	if (contrasena == "smith" || contrasena == "neo") {
+	if (contrasena == "neo") {
 		system("COLOR 02");
 
 		char caRow[80];
@@ -54,12 +54,54 @@ int main() {
 			}
 			
 			if (i <1002);{
-				cout << "El Sistema Fallo";
+				cout << "\n El Sistema Fallo" << endl;
 				break;
 			}
 		}		
-	} else {
-		cout << "acceso a la matrix denegado";
-	}
+	} 
+		
+
+	
+	if (contrasena == "smith") {
+		system("COLOR 02");
+
+		char caRow[80];
+		int j = 7;
+		int k = 2;
+		int l = 5;
+		int m = 1;
+
+		while (true) {
+			int i = 0;
+			
+			// caracteres aleatorios
+			while (i < 80) {
+				if (caRow[i] != ' ') {
+					caRow[i] = GetChar(j + i*i, 33, 30);
+				}
+				std::cout << caRow[i];
+				++i;
+			}
+			j = (j + 31);
+			k = (k + 17);
+			l = (l + 47);
+			m = (m + 67);
+			caRow[Modulus(j, 80)] = '-';
+			caRow[Modulus(k, 80)] = ' ';
+			caRow[Modulus(l, 80)] = '-';
+			caRow[Modulus(m, 80)] = ' ';
+			
+			// incremente el valor de 3000000 para retrasar el proceso
+			i = 0;
+			while (i < 3000000) {
+				GetChar(1, 1, 1);
+				 ++i;
+			}
+			
+			if (i < 1002);{
+			}
+		}		
+	} 
+
     return 0;
 }
